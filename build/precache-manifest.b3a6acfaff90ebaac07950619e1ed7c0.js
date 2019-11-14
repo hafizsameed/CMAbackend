6b9aa396add0f6dea8789b76435844e006c8cfc9
hafizsameed <hafizsameed42@gmail.com>
2019-11-14 13:15:03 +0500

    finalized with build

---
+++ build/precache-manifest.b3a6acfaff90ebaac07950619e1ed7c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ec88bf12510b6e7a8146a9bd48415b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ca26d019934ae8cdad1",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "4be60842b5db1e3705e0",
+    "url": "/static/css/main.29643d12.chunk.css"
+  },
+  {
+    "revision": "3ca26d019934ae8cdad1",
+    "url": "/static/js/2.d2e89131.chunk.js"
+  },
+  {
+    "revision": "4be60842b5db1e3705e0",
+    "url": "/static/js/main.e44a2b82.chunk.js"
+  },
+  {
+    "revision": "2e4bc1b3c022e2e6153b",
+    "url": "/static/js/runtime-main.d54b910c.js"
+  }
+]);
